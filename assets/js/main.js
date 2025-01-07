f86f04edca3ebdc43a2d489bea17b86110047bb4
susheeljii <b230034@gmail.com>
2025-01-07 16:17:24 +0530

    Changes made

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (ithis.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListner('scroll', scrollHeader)
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
